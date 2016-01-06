63c3cb1004c2da7fd24958e3c07e5bfa1cf23dbb
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2016-01-06 20:30:41 +1000

    Add abc subpackage

---
+++ cafe/abc/__init__.py
@@ -0,0 +1,10 @@
+from abc import ABCMeta
+
+
+class AbstractClass(object):
+    __metaclass__ = ABCMeta
+
+    @classmethod
+    def __subclasshook__(cls, other_class):
+        return super(AbstractClass, cls).__subclasshook__(other_class) and all(
+                any(x in B.__dict__ for B in other_class.__mro__) for x in cls.__abstractmethods__)
