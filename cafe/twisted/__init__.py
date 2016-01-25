62dd2fec1c460aa76521e054474b9110903ad839
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2016-01-25 17:53:32 +1000

    patterns: add twisted patterns

---
+++ cafe/twisted/__init__.py
@@ -0,0 +1,17 @@
+
+try:
+    from twisted.internet import defer, reactor
+
+    def async_sleep(seconds):
+        """
+        An asynchronous sleep function using twsited.
+
+        Source: http://twistedmatrix.com/pipermail/twisted-python/2009-October/020788.html
+
+        :type seconds: int
+        """
+        d = defer.Deferred()
+        reactor.callLater(seconds, d.callback, seconds)
+        return d
+except ImportError:
+    pass
