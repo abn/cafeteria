dace2ce8261c52cb4ca83cc842c88e13ee4ddfcf
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2016-03-18 12:22:39 +1000

    units: add data and data rate unit support

---
+++ cafe/datastructs/units/__init__.py
@@ -0,0 +1,34 @@
+from re import match
+
+
+try:
+    long
+except NameError:
+    long = int
+
+
+class BaseUnitClass(float):
+    UNITS = {}
+
+    # noinspection PyInitNewSignature
+    def __new__(cls, x, unit=None):
+        if isinstance(x, str):
+            units_regex = '|'.join(cls.UNITS.keys())
+            m = match('^(\d+(.\d+)?) ?({})$'.format(units_regex), x)
+            if m is None:
+                raise ValueError('{} requires number or a string in the format "<value> ({})"'.format(
+                    cls.__name__, units_regex))
+            x = float(m.group(1)) * cls.UNITS.get(m.group(3))
+        elif unit is None:
+            raise ValueError('No unit provided.')
+        else:
+            x = x * cls.UNITS[unit]
+        return super(BaseUnitClass, cls).__new__(cls, x)
+
+    def __getattr__(self, item):
+        if item in self.UNITS:
+            # if unit is known convert to unit
+            result = self * 1.0 / self.UNITS[item]
+            rounded = long(result)
+            return result if result != rounded else rounded
+        raise AttributeError('{} is not a valid conversion unit'.format(item))
---
+++ cafe/datastructs/units/data.py
@@ -0,0 +1,67 @@
+from enum import Enum
+
+from cafe.datastructs.units import BaseUnitClass
+
+
+class DataMultiplier(Enum):
+    k = 10 ** 3
+    M = 10 ** 6
+    G = 10 ** 9
+    T = 10 ** 12
+    P = 10 ** 15
+    E = 10 ** 18
+    Z = 10 ** 21
+    Y = 10 ** 24
+    Ki = 2 ** 10
+    Mi = 2 ** 20
+    Gi = 2 ** 30
+    Ti = 2 ** 40
+    Pi = 2 ** 50
+    Ei = 2 ** 60
+
+
+class DataBaseUnit(Enum):
+    b = 1
+    bit = 1
+    B = 8
+    byte = 8
+
+
+class DataUnit(BaseUnitClass):
+    """
+    A data unit object internally stores the number of bits associated.
+    Eg: DataUnit(1, 'byte') == 8
+    """
+    UNITS = {
+        '{}{}'.format(multiplier, base):
+            DataMultiplier[multiplier].value * DataBaseUnit[base].value
+        for multiplier in DataMultiplier.__members__
+        for base in DataBaseUnit.__members__
+    }
+    UNITS.update({
+        base: DataBaseUnit[base].value
+        for base in DataBaseUnit.__members__
+    })
+
+    # noinspection PyInitNewSignature
+    def __new__(cls, x, unit=None):
+        if unit is None:
+            unit = DataBaseUnit.bit.name
+        return super(DataUnit, cls).__new__(cls, x, unit)
+
+
+class DataRateUnit(DataUnit):
+    """
+    A data rate unit object internally stores the number bits per second.
+    """
+    UNITS = {
+        '{}{}'.format(unit, suffix): DataUnit.UNITS[unit]
+        for unit in DataUnit.UNITS
+        for suffix in ['/s', 'ps']
+    }
+
+    # noinspection PyInitNewSignature
+    def __new__(cls, x, unit=None):
+        if unit is None:
+            unit = '{}ps'.format(DataBaseUnit.bit.name)
+        return super(DataUnit, cls).__new__(cls, x, unit)
